22957222911f64b68860f10ce4c06c3b8784378e
Ferid Amir <93754712+nubiemir@users.noreply.github.com>
2023-11-15 16:39:48 +0400

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,43 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: famir <famir@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/07 19:56:42 by famir             #+#    #+#             */
-/*   Updated: 2023/10/30 19:36:41 by famir            ###   ########.fr       */
+/*   Created: 2023/10/07 20:00:20 by famir             #+#    #+#             */
+/*   Updated: 2023/10/30 20:01:48 by famir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-unsigned int ft_strlen(const char *s)
+int find_line(char *str)
 {
-	unsigned int res;
-	unsigned int counter;
+    int counter = 0;
 
-	counter = 0;
-	res = 0;
-	while (s[counter])
-	{
-		counter++;
-		res++;
-	}
-	return (res);
+    while (str[counter] && str[counter] != '\n')
+        counter++;
+    return counter;
 }
 
-t_bool is_empty(t_queue *queue)
+char *get_next_line(int fd)
 {
-	if (!queue)
-		return (0);
-	if (queue->top == -1)
-		return (0);
-	return (1);
-}
-void enqueue(t_queue *queue, void *data)
-{
-}
-void dequeue(t_queue *queue)
-{
+    // static char *file;
+    char *res;
+
+    res = malloc((BUFFER_SIZE) * sizeof(char));
+    read(fd, res, BUFFER_SIZE);
+
+    printf("read buffer: %s\n", res);
+    return (res);
 }
