9f7eea3f09d6dc58a2a2ecb6e50ce9adc8927c7a
Ferid Amir <93754712+nubiemir@users.noreply.github.com>
2023-11-16 12:24:10 +0400

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -27,17 +27,48 @@ unsigned int ft_strlen(const char *s)
 	return (res);
 }
 
+t_queue *create_queue()
+{
+	t_queue *queue;
+
+	queue = (t_queue *)malloc(sizeof(t_queue));
+	if (!queue)
+		return (NULL);
+	queue->front = queue->rear = NULL;
+	return queue;
+}
+
 t_bool is_empty(t_queue *queue)
 {
-	if (!queue)
-		return (0);
-	if (queue->top == -1)
-		return (0);
-	return (1);
+	return (queue->front == NULL);
 }
 void enqueue(t_queue *queue, void *data)
 {
+	t_node *new_node;
+
+	new_node = (t_node *)malloc(sizeof(t_node));
+	new_node->data = data;
+	new_node->next = NULL;
+
+	if (is_empty(queue))
+	{
+		queue->front = queue->rear = new_node;
+	}
+	else
+	{
+		queue->rear->next = new_node;
+		queue->rear = new_node;
+	}
 }
-void dequeue(t_queue *queue)
+void *dequeue(t_queue *queue)
 {
+	void *data;
+
+	if (!queue || is_empty(queue))
+		return (NULL);
+	data = queue->front->data;
+	queue->front = queue->front->next;
+	if (queue->front == NULL)
+		queue->rear = NULL;
+	return (data);
 }
